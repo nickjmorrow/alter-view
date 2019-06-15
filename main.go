50ceb4a42daa4810a8a191c336148c06741220cf
Nicholas Morrow <njmorrow95@gmail.com>
2019-06-15 11:24:11 -0400

    Before trying graphql handler

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"github.com/nickjmorrow/blog/models"
 	"github.com/rs/cors"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Animal struct {
@@ -20,6 +25,10 @@ type Animal struct {
 	Name     string
 }
 
+func (animal Animal) TableName() string {
+	return "animals"
+}
+
 type Article struct {
 	gorm.Model
 
@@ -27,8 +36,64 @@ type Article struct {
 	Name      string
 }
 
-func (animal Animal) TableName() string {
-	return "animals"
+type Todo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+}
+
+var TodoList []Todo
+
+func init() {
+	todo1 := Todo{ID: "a", Text: "A todo not to forget", Done: false}
+	todo2 := Todo{ID: "b", Text: "This is the most important", Done: false}
+	todo3 := Todo{ID: "c", Text: "Please do this or else", Done: false}
+	TodoList = append(TodoList, todo1, todo2, todo3)
+
+	rand.Seed(time.Now().UnixNano())
+}
+
+var todoType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Todo",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"text": &graphql.Field{
+			Type: graphql.String,
+		},
+		"done": &graphql.Field{
+			Type: graphql.Boolean,
+		},
+	},
+})
+
+var rootQuery = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootQuery",
+	Fields: graphql.Fields{
+		"todoList": &graphql.Field{
+			Type:        graphql.NewList(todoType),
+			Description: "List of todos",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return TodoList, nil
+			},
+		},
+	},
+})
+
+var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query: rootQuery,
+})
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("Wrong result, unexpected errors: %v", result.Errors)
+	}
+	return result
 }
 
 var db *gorm.DB
@@ -54,10 +119,25 @@ func main() {
 	router.HandleFunc("/animals/{id}", GetAnimal).Methods("GET")
 	router.HandleFunc("/animals", CreateResource).Methods("POST")
 	router.HandleFunc("/animals/{id}", DeleteResource).Methods("DELETE")
+	router.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s", b)
+		// fmt.Print(r.Body)
+		// fmt.Print(r.URL)
+		// fmt.Print(r.URL.Query())
+		s := string(b)
+		result := executeQuery(s, schema)
+		// result := executeQuery(r.URL.Query().Get("query"), schema)
+		json.NewEncoder(w).Encode(result)
+	})
 
 	handler := cors.Default().Handler(router)
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), handler))
+	fmt.Println("Listening on ", port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 
 }
 
